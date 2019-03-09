242bdc89f290dcc4b22a7a1fbf0a2658583aa673
root <root@localhost.localdomain>
2019-03-10 00:27:17 +0530

    Finding particular Window fn2

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ typedef struct flight_tag{
 }flight;
 
 typedef struct window_tag{
-  char window_id[4];
+  char window_id[3];
   struct flight *fptr;
   struct window_tag *next;
   struct window_tag *prev;
@@ -44,11 +44,14 @@ flight* initialiseFlight(){
   fptr -> next = NULL;
   return fptr;
 }
-window* windowIAS(window **wpptr,char f_arr[][], int r, int c)
+void addFlightServiceWindow(window **wpptr,char f_arr[][], int r, int c)
 {
   window *temp,*ptr,*wptr;
   ptr = *wpptr;
   wptr = (window*)malloc(sizeof(window));
+  wptr -> window_id[0] = f_arr[0][0];
+  wptr -> window_id[1] = f_arr[0][1];
+  wptr -> window_id[2] = f_arr[0][2];
   wptr -> fptr = NULL;
   for (int i = 0; i < r; i++) {
     nptr = initialiseFlight();
@@ -56,11 +59,46 @@ window* windowIAS(window **wpptr,char f_arr[][], int r, int c)
     strcpy(nptr -> flight_id,f_arr[i]);
     wptr -> fptr = nptr;
   }
-  ptr 
-  nptr->data=d;
-  nptr->next=ptr;
-  lptr=nptr;
-  return lptr;
+  wptr -> next = ptr;
+  if (!ptr) {
+    ptr -> prev = wptr;
+  }
+  *wpptr = wptr;
+  // return wptr;
+}
+void allocateBoardingPass(window **wpptr,char flight_id[], char ticket_id[]){
+  char w_id[3], f_id[2];
+  int flag = 0, w, wf;
+  window *wptr;
+  wptr = *wpptr;
+  w_id[0] = ticket_id[0];
+  w_id[1] = ticket_id[1];
+  w_id[2] = ticket_id[2];
+  f_id[0] = ticket_id[3];
+  f_id[1] = ticket_id[4];
+  while (wptr && flag == 0) {
+    w = strcmp(w_id, wptr -> window_id);
+    if (w == 0) {
+      while (wptr -> fptr && flag == 0) {
+        wf = strcmp(f_id, (wptr -> fptr) -> flight_id);
+        if(wf == 0){
+          flag = 1;
+        }
+        if (flag == 0) {
+          wptr -> fptr = (wptr -> fptr) -> next;
+        }
+      }
+    }
+    if (flag == 0) {
+      wptr = wptr -> next;
+    }
+  }
+  if (flag == 0) {
+    printf("We can't find a suitable flight on the provided Details \n");
+  }
+  else if (flag == 1) {
+    
+  }
 }
 node_type* iaS(node_type **lpptr,int d)
 {

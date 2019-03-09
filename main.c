5c5184a570b51a769009dc824e8193f2bb89723b
root <root@localhost.localdomain>
2019-03-10 01:49:37 +0530

    Boarding Pass

--- main.c
+++ main.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 
-typedef struct ticket_tag{
-  char ticket_id[10];
-  int noOfPassengers;
-}ticket;
+// typedef struct ticket_tag{
+//   char ticket_id[10];
+//   int noOfPassengers;
+// }ticket;
 
 typedef struct passenger_tag{
   char pass_name[20];
-  struct ticket tic;
+  // struct ticket tic;
+  char ticket_id[10];
   int age;
   int luggage_weight;
   int fine;
   int seat_no;
-  int noOfpassAssociated;
+  // int noOfpassAssociated;
   struct passenger_tag *next;
 }passenger;
 
@@ -31,7 +32,35 @@ typedef struct window_tag{
   struct window_tag *prev;
 }window;
 
-
+passenger* initialisePassenger(){
+  passenger *fptr;
+  fptr = (passenger*)malloc(sizeof(passenger));
+  fptr -> pass_name[0] = '\0';
+  fptr -> ticket_id[0] = '\0';
+  fptr -> age = 0;
+  fptr -> luggage_weight = 0;
+  fptr -> fine = 0;
+  fptr -> seat_no = 0;
+  // fptr -> noOfpassAssociated = 0;
+  fptr -> next = NULL;
+  return fptr;
+}
+passenger* getPassenerDetails(){
+  passenger *fptr;
+  // char name[20];
+  fptr = initialisePassenger();
+  printf("Enter Name of Passenger: ");
+  scanf("%s", fptr -> pass_name);
+  printf("Enter age: ");
+  scanf("%d", fptr -> age);
+  printf("Enter the weight of luggage: ");
+  scanf("%d", fptr -> luggage);
+  if (fptr -> luggage > 15) {
+    /* code */
+    fptr -> fine = 100 * ( (fptr -> luggage) - 15 );
+  }
+  return fptr;
+}
 flight* initialiseFlight(){
   flight *fptr;
   fptr = (flight*)malloc(sizeof(flight));
@@ -68,6 +97,7 @@ void addFlightServiceWindow(window **wpptr,char f_arr[][], int r, int c)
 }
 void allocateBoardingPass(window **wpptr,char flight_id[], char ticket_id[]){
   char w_id[3], f_id[2];
+  passenger *pass, *temp, *pre_ptr;
   int flag = 0, w, wf;
   window *wptr;
   wptr = *wpptr;
@@ -97,7 +127,53 @@ void allocateBoardingPass(window **wpptr,char flight_id[], char ticket_id[]){
     printf("We can't find a suitable flight on the provided Details \n");
   }
   else if (flag == 1) {
-    
+    pre_ptr = (wptr -> fptr) -> pptr;
+    printf("No of Passengers: ");
+    scanf("%d", &p);
+    if (p > 4 || p < 1) {
+      printf("Enter proper no. of passengers (0<p<5)\n");
+    }
+    else{
+      pass = getPassengerDetails();
+      if ((pptr -> age) > 60) {
+        for (int i = 0; i < 60; i++) {
+          if ((((wptr -> fptr) -> seats[i]) % 2 == 0 || ((wptr -> fptr) -> seats[i]) % 3 == 0 ) && ((wptr -> fptr) -> seats[i]) == 0) {
+            (wptr -> fptr) -> seats[i] = 1;
+            pptr -> seat_no = i + 1;
+          }
+        }
+      }
+      else if ((pptr -> age) < 15) {
+        for (int i = 0; i < 60; i++) {
+          if ((((wptr -> fptr) -> seats[i]) % 6 == 0 || ((wptr -> fptr) -> seats[i]) % 5 == 0 ) && ((wptr -> fptr) -> seats[i]) == 0) {
+            (wptr -> fptr) -> seats[i] = 1;
+            pptr -> seat_no = i + 1;
+          }
+        }
+      }
+      else{
+        for (int i = 0; i < 60; i++) {
+          if (((wptr -> fptr) -> seats[i]) == 0) {
+            (wptr -> fptr) -> seats[i] = 1;
+            pptr -> seat_no = i + 1;
+          }
+        }
+      }
+      int s = i;
+      pass -> next = pre_ptr;
+      if (p != 1) {
+        for (int q = 1; q < p; q++) {
+          temp = getPassengerDetails();
+          if (((wptr -> fptr) -> seats[s]) == 0) {
+            (wptr -> fptr) -> seats[s] = 1;
+            temp -> seat_no = s + 1;
+            temp -> next = pass;
+            pass = temp;
+          }
+        }
+      }
+      wptr -> fptr -> pptr = pass;
+    }
   }
 }
 node_type* iaS(node_type **lpptr,int d)
